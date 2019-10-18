e2bac1b9f633a85c3d496ed7b9fed565f9b8cbc8
aaabfly <094m427@gmail.com>
2019-10-18 11:30:46 +0900

    10/18 課題

--- lib/enemy.rb
+++ lib/enemy.rb
@@ -1,1 +1,23 @@
 # 敵 (Enemy) のふるまいを表すクラス
+attr_accessor :hp, :mp
+attr_reader :name, :magic, :power
+
+require_relative 'hero'
+
+clas Enemy
+  def initialize
+    @name = "コボルト"
+    @hp = 40
+    @mp = 10
+    @magic = 10
+    @power = 10
+  end
+
+  def attack
+    Hero.hp -= power * rand(1..2)
+  end
+
+  def dead?
+    @hp <= 0
+  end
+
--- lib/hero.rb
+++ lib/hero.rb
@@ -1,1 +1,23 @@
 # 勇者 (Hero) のふるまいを表すクラス
+attr_accessor :hp, :mp
+attr_reader :name, :magic, :power
+
+require_relative 'enemy'
+
+clas Here
+  def initialize()
+    @name = "アイク"
+    @hp = 80
+    @mp = 80
+    @magic = 20
+    @power = 10
+  end
+
+  def magic_attack
+    Enemy.hp -= magic * rand(1..4)
+    @mp -= 10
+  end
+
+  def dead?
+    @hp <= 0
+  end
